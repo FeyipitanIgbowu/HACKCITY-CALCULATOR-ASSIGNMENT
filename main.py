81358fa287e7a1681acd1931fd830bb64e3e3c79
Feyipitan Igbowu <102683425+Techsirius1@users.noreply.github.com>
2022-10-30 10:16:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+
+print("WELCOME TO SIMPLE CALCULATOR")
+first_value = input("Enter the first value: ")
+second_value = input("Enter the second value: ")
+first_value_change = int(first_value)
+second_value_change = int(second_value)
+
+addition = first_value_change + second_value_change
+print(addition)
+
+subtratction = first_value_change - second_value_change
+print(subtratction)
+
+multiplication = first_value_change * second_value_change
+print(multiplication)
+
+division = first_value_change / second_value_change
+print(division)
+
+import math
+exponential = math.exp(addition)
+print(exponential)
+
+square_root = math.sqrt(addition)
+print(square_root)
